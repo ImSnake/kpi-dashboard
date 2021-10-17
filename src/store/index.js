1014b5697fa48e6609d7fead9ce9fe8e35d53b39
Yuliya Pulikova <pulikova.j@gmail.com>
2021-10-17 11:43:45 +0300

    initial

--- src/store/index.js
+++ src/store/index.js
@@ -2,151 +2,151 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-/*    income: [
+    income: [
       {
-      '2017': {
-        'plan': {
+      "2017": {
+        "plan": {
           q1: {
-            'integrator': 0,
-            'partner': 0,
-            'distributor': 0,
-            'partners': 0,
-            'c3d-labs': 0,
-            'other': 0
+            "integrator": 0,
+            "partner": 0,
+            "distributor": 0,
+            "partners": 0,
+            "c3d-labs": 0,
+            "other": 0
           } ,
           q2: {
-            'integrator': 0,
-            'partner': 0,
-            'distributor': 0,
-            'partners': 0,
-            'c3d-labs': 0,
-            'other': 0
+            "integrator": 0,
+            "partner": 0,
+            "distributor": 0,
+            "partners": 0,
+            "c3d-labs": 0,
+            "other": 0
           } ,
           q3: {
-            'integrator': 0,
-            'partner': 0,
-            'distributor': 0,
-            'partners': 0,
-            'c3d-labs': 0,
-            'other': 0
+            "integrator": 0,
+            "partner": 0,
+            "distributor": 0,
+            "partners": 0,
+            "c3d-labs": 0,
+            "other": 0
           } ,
           q4: {
-            'integrator': 0,
-            'partner': 0,
-            'distributor': 0,
-            'partners': 0,
-            'c3d-labs': 0,
-            'other': 0
+            "integrator": 0,
+            "partner": 0,
+            "distributor": 0,
+            "partners": 0,
+            "c3d-labs": 0,
+            "other": 0
           } ,
         },
-        'fact': {
+        "fact": {
           q1: {
-            'integrator': 0,
-            'partner': 0,
-            'distributor': 0,
-            'partners': 0,
-            'c3d-labs': 0,
-            'other': 0
+            "integrator": 0,
+            "partner": 0,
+            "distributor": 0,
+            "partners": 0,
+            "c3d-labs": 0,
+            "other": 0
           } ,
           q2: {
-            'integrator': 0,
-            'partner': 0,
-            'distributor': 0,
-            'partners': 0,
-            'c3d-labs': 0,
-            'other': 0
+            "integrator": 0,
+            "partner": 0,
+            "distributor": 0,
+            "partners": 0,
+            "c3d-labs": 0,
+            "other": 0
           } ,
           q3: {
-            'integrator': 0,
-            'partner': 0,
-            'distributor': 0,
-            'partners': 0,
-            'c3d-labs': 0,
-            'other': 0
+            "integrator": 0,
+            "partner": 0,
+            "distributor": 0,
+            "partners": 0,
+            "c3d-labs": 0,
+            "other": 0
           } ,
           q4: {
-            'integrator': 0,
-            'partner': 0,
-            'distributor': 0,
-            'partners': 0,
-            'c3d-labs': 0,
-            'other': 0
+            "integrator": 0,
+            "partner": 0,
+            "distributor": 0,
+            "partners": 0,
+            "c3d-labs": 0,
+            "other": 0
           } ,
         }
         }
       }, {
-        '2018': {
-          'plan': {
+        "2020": {
+          "plan": {
             q1: {
-              'integrator': 0,
-              'partner': 0,
-              'distributor': 0,
-              'partners': 0,
-              'c3d-labs': 0,
-              'other': 0
+              "integrator": 0,
+              "partner": 0,
+              "distributor": 0,
+              "partners": 0,
+              "c3d-labs": 0,
+              "other": 0
             },
             q2: {
-              'integrator': 0,
-              'partner': 0,
-              'distributor': 0,
-              'partners': 0,
-              'c3d-labs': 0,
-              'other': 0
+              "integrator": 0,
+              "partner": 0,
+              "distributor": 0,
+              "partners": 0,
+              "c3d-labs": 0,
+              "other": 0
             },
             q3: {
-              'integrator': 0,
-              'partner': 0,
-              'distributor': 0,
-              'partners': 0,
-              'c3d-labs': 0,
-              'other': 0
+              "integrator": 0,
+              "partner": 0,
+              "distributor": 0,
+              "partners": 0,
+              "c3d-labs": 0,
+              "other": 0
             },
             q4: {
-              'integrator': 0,
-              'partner': 0,
-              'distributor': 0,
-              'partners': 0,
-              'c3d-labs': 0,
-              'other': 0
+              "integrator": 0,
+              "partner": 0,
+              "distributor": 0,
+              "partners": 0,
+              "c3d-labs": 0,
+              "other": 0
             },
           },
-          'fact': {
+          "fact": {
             q1: {
-              'integrator': 0,
-              'partner': 0,
-              'distributor': 0,
-              'partners': 0,
-              'c3d-labs': 0,
-              'other': 0
+              "integrator": 0,
+              "partner": 0,
+              "distributor": 0,
+              "partners": 0,
+              "c3d-labs": 0,
+              "other": 0
             },
             q2: {
-              'integrator': 0,
-              'partner': 0,
-              'distributor': 0,
-              'partners': 0,
-              'c3d-labs': 0,
-              'other': 0
+              "integrator": 0,
+              "partner": 0,
+              "distributor": 0,
+              "partners": 0,
+              "c3d-labs": 0,
+              "other": 0
             },
             q3: {
-              'integrator': 0,
-              'partner': 0,
-              'distributor': 0,
-              'partners': 0,
-              'c3d-labs': 0,
-              'other': 0
+              "integrator": 0,
+              "partner": 0,
+              "distributor": 0,
+              "partners": 0,
+              "c3d-labs": 0,
+              "other": 0
             },
             q4: {
-              'integrator': 0,
-              'partner': 0,
-              'distributor': 0,
-              'partners': 0,
-              'c3d-labs': 0,
-              'other': 0
+              "integrator": 0,
+              "partner": 0,
+              "distributor": 0,
+              "partners": 0,
+              "c3d-labs": 0,
+              "other": 0
             },
           }
         }
       }
-    ],*/
+    ],
   },
   mutations: {},
   actions: {},
